cbbc2170ebf4cd9d9c78443391df3c1ad72d7283
nuttayosc <122503568+nuttayosc@users.noreply.github.com>
2023-01-14 14:16:04 +0700

    Add files via upload

---
+++ ex2 CDLinkedList/src/CDLinkedList.java
@@ -0,0 +1,203 @@
+public class CDLinkedList {
+	DListNode header;
+	int size;
+	static final int HEADERVALUE = -9999999;
+
+	public CDLinkedList() {
+		header = new DListNode(HEADERVALUE);
+		makeEmpty();// necessary, otherwise next/previous node will be null
+	}
+
+	public boolean isEmpty() {
+		return header.nextNode == header;
+	}
+
+	public boolean isFull() {
+		return false;
+	}
+
+	/** make the list empty. */
+	public void makeEmpty() {
+		header.nextNode = header;
+		header.previousNode = header;
+		size =0;
+	}
+
+	// put in new data after the position of p.
+	public void insert(int value, Iterator p) throws Exception {
+		if (p == null || !(p instanceof DListIterator))
+			throw new Exception();
+		DListIterator p2 = (DListIterator) p;
+		if (p2.currentNode == null)
+			throw new Exception();
+
+		DListIterator p3 = new DListIterator(p2.currentNode.nextNode);
+		DListNode n = new DListNode(value, p3.currentNode, p2.currentNode);
+		p2.currentNode.nextNode = n;
+		p3.currentNode.previousNode = n;
+		size++;
+	}
+
+	// return position number of value found in the list.
+	// otherwise, return -1.
+	public int find(int value) throws Exception {
+		DListIterator itr = new DListIterator(header);
+		int index = -1;
+		while (itr.hasNext()) {
+			int v = itr.next();
+			index++;
+			if (itr.currentNode == header)
+				return -1;
+			if (v == value)
+				return index; // return the position of value.
+		}
+		return -1;
+	}
+
+	// return data stored at kth position.
+	public int findKth(int kthPosition) throws Exception {
+		if (kthPosition < 0 || kthPosition > size-1)
+			throw new Exception();// exit the method if the position is
+		// beyond the first/last possible
+		// position, throwing exception in the process.
+		DListIterator itr = new DListIterator(header);
+		int index = -1;
+		while (itr.hasNext()) {
+			int v = itr.next();
+			index++;
+			if (itr.currentNode == header)
+				throw new Exception();
+			if (index == kthPosition)
+				return v;
+		}
+		throw new Exception();
+	}
+
+	// Return iterator at position before the first position that stores value.
+	// If the value is not found, return null.
+	public Iterator findPrevious(int value) throws Exception {
+		if (isEmpty())
+			return null;
+		Iterator itr1 = new DListIterator(header);
+		Iterator itr2 = new DListIterator(header);
+		int currentData = itr2.next();
+		while (currentData != value) {
+			currentData = itr2.next();
+			itr1.next();
+			if (((DListIterator) itr2).currentNode == header)
+				return null;
+		}
+		return itr1;
+	}
+
+	// remove content at position just after the given iterator. Skip header if
+	// found.
+	public void remove(Iterator p) {
+		if (isEmpty())
+			return;
+		if (p == null || !(p instanceof DListIterator))
+			return;
+		DListIterator p2 = (DListIterator) p;
+		if (p2.currentNode == null)
+			return;
+		if (p2.currentNode.nextNode == header)
+			p2.currentNode = header;
+		if (p2.currentNode.nextNode == null)
+			return;
+		DListIterator p3 = new DListIterator(p2.currentNode.nextNode.nextNode);
+		p2.currentNode.nextNode = p3.currentNode;
+		p3.currentNode.previousNode = p2.currentNode;
+		size--;
+	}
+
+	// remove the first instance of the given data.
+	public void remove(int value) throws Exception {
+		Iterator p = findPrevious(value);
+		if (p == null)
+			return;
+		remove(p);
+	}
+
+	// remove data at position p.
+	// if p points to header or the list is empty, do nothing.
+	public void removeAt(Iterator p) throws Exception {
+		if (isEmpty() || p == null || !(p instanceof DListIterator) || ((DListIterator) p).currentNode == null
+				|| ((DListIterator) p).currentNode == header)
+			return;
+
+		DListIterator p2 = (DListIterator) (findPrevious(p));
+		remove(p2);
+
+	}
+
+	// Print each contact out, one by one.
+	// To be completed by students.
+	public void printList() throws Exception {
+		Iterator itr = new DListIterator(header);
+		while (itr.hasNext()) {
+			Object data = itr.next();
+
+			System.out.println(data);
+
+		}
+	}
+
+	public int size() throws Exception {
+		return size;
+	}
+
+	// return iterator pointing to location before position.
+	public Iterator findPrevious(Iterator position) throws Exception {
+		if (position == null)
+			return null;
+		if (!(position instanceof DListIterator))
+			return null;
+		if (((DListIterator) position).currentNode == null)
+			return null;
+
+		DListIterator p = ((DListIterator) position);
+		DListIterator p2 = new DListIterator(p.currentNode.previousNode);
+		return p2;
+
+	}
+	
+	public void swapRange(Iterator s1, Iterator f1, Iterator s2, Iterator f2) throws Exception {
+		// null and not DListIterator
+		if (!(s1 instanceof DListIterator) || !(s2 instanceof DListIterator) || !(f1 instanceof DListIterator) || !(f2 instanceof DListIterator) || s1 == null || s2 == null || f1 == null || f2 == null){
+            return;
+        }
+		
+		// Iterator 
+        DListIterator st1 = (DListIterator) s1;
+        DListIterator st2 = (DListIterator) s2;
+        DListIterator fl1 = (DListIterator) f1;
+        DListIterator fl2 = (DListIterator) f2;
+        
+        //swap start
+        DListIterator st1_previous = (DListIterator) findPrevious(st1);
+        DListIterator st2_previous = (DListIterator) findPrevious(st2);
+        
+        // st2prev <-- st1  && st2prev --> st1
+        st1.currentNode.previousNode = st2_previous.currentNode;
+        st2_previous.currentNode.nextNode = st1.currentNode;
+        
+        // st1prev <-- st2  && st1prev --> st2
+        st2.currentNode.previousNode = st1_previous.currentNode;
+        st1_previous.currentNode.nextNode = st2.currentNode;
+        
+        //swap end
+        DListIterator fl1_next = new DListIterator (fl1.currentNode.nextNode);
+        DListIterator fl2_next = new DListIterator (fl2.currentNode.nextNode);
+        
+        // fl1 --> fl2next && fl1 <-- fl2next
+        fl1.currentNode.nextNode = fl2_next.currentNode;
+        fl2_next.currentNode.previousNode = fl1.currentNode;
+        
+        // fl2 --> fl1next && fl2 <-- fl1next
+        fl2.currentNode.nextNode = fl1_next.currentNode;
+        fl1_next.currentNode.previousNode = fl2.currentNode;
+        
+        return;
+	}
+
+}
